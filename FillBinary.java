730d52bc11929b7e20ee7fa9ede730208267075f
AlexsandrPavlov <buterfeed@gmail.com>
2024-08-12 14:19:19 +0300

    Delete FillBinary.java

--- FillBinary.java
+++
@@ -1,8 +0,0 @@
-public class FillBinary extends main{
-
-    public static void fillbinary(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i + 1; // Заполняем массив начиная с 1
-        }
-    }
-}
