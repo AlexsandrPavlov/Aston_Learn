9cebc8dca3ce79e9ab2e341a96d48256822022da
AlexsandrPavlov <buterfeed@gmail.com>
2024-08-12 14:17:37 +0300

    Delete RepeatString.java

--- RepeatString.java
+++
@@ -1,8 +0,0 @@
-public class RepeatString extends main{
-
-    public static void repeatString(String str, int count) {
-        for (int i = 0; i < count; i++) {
-            System.out.println(str);
-        }
-    }
-}
