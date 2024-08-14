dc01a303e1b5a7fad7bec0630f60eac3d4fd51b6
Alexsandr Pavlov <buterfeed@gmail.com>
2024-08-14 11:53:30 +0300

    Урок 12 Астон

--- Main.java
+++ Main.java
@@ -1,45 +1,44 @@
 public class Main {
+
     public static void main(String[] args) {
 
-        Cat barsik = new Cat("Барсик");
-        Dog tuzik = new Dog("Тузик");
-        Cat bashmak = new Cat("Башмак");
-        Dog sobaken = new Dog("Собакен");
-        barsik.run(150);
-        bashmak.swim(27);
-        tuzik.run(500);
-        sobaken.swim(15);
-        System.out.println("Всего животных " +Animal.getCountAnimal());
-        System.out.println("Всего котов "+Cat.getCountCat());
-        System.out.println("Всего псов "+Dog.getCountDod());
-
-
-        Kitchen kitchen = new Kitchen(10);
-        Cat cat1 = new Cat("Васька");
-        Cat cat2 = new Cat("Барсик");
-        Cat cat3 = new Cat("Муся");
-
-        kitchen.addCat(cat1);
-        kitchen.addCat(cat2);
-        kitchen.addCat(cat3);
-
-        kitchen.feedCats();
-        kitchen.printSatiationStatus();
-
-
-        GeometricShape circle = new Circle(5);
-        circle.setFillColor("red");
-        circle.setBorderColor("black");
-        circle.printInfo();
-
-        GeometricShape rectangle = new Rectangle(10, 5);
-        rectangle.setFillColor("green");
-        rectangle.setBorderColor("blue");
-        rectangle.printInfo();
-        GeometricShape triangle = new Triangle(8, 6);
-        triangle.setFillColor("yellow");
-        triangle.setBorderColor("purple");
-        triangle.printInfo();
+        String[][] arr = new String[][]{{"1", "9", "2", "7"}, {"в", "8", "а", "3"}, {"1", "5", "2", "2"}, {"6", "7", "9", "1"}};
+        try {
+            try {
+                int result = method(arr);
+                System.out.println(result);
+            } catch (MyArraySizeException e) {
+                System.out.println("Размер массива неверен");
+            }
+        }
+        catch (MyArrayDataException e) {
+            System.out.println("Неправильное значение массива!");
+            System.out.println("Ошибка в ячейке: " + e.i + "x" + e.j);
+        }
 
     }
+
+
+    public static int method(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        int count = 0;
+        if (arr.length != 4) {
+            throw new MyArraySizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    count = count + Integer.parseInt(arr[i][j]);
+                }
+                catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+
+        }
+        return count;
+    }
+
 }
